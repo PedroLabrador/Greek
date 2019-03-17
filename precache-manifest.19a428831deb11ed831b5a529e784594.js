f0d7cd194cd72e2dce56eb3f02f3fa99cba02c56
PedroLabrador <pedrolabrador3@outlook.com>
2019-03-17 18:24:44 -0400

    Updates

---
+++ precache-manifest.19a428831deb11ed831b5a529e784594.js
@@ -0,0 +1,314 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6109e4d61cc9d7b6168a1f72bbb9d31",
+    "url": "/greek/static/media/Anim11.b6109e4d.gif"
+  },
+  {
+    "revision": "4fdd942419c1e27ef3ef",
+    "url": "/greek/static/css/main.324a4a45.chunk.css"
+  },
+  {
+    "revision": "5555ee16424ee8b2d39a",
+    "url": "/greek/static/js/runtime~main.bea1a583.js"
+  },
+  {
+    "revision": "cdadf99eb97bf39b5d66",
+    "url": "/greek/static/js/2.f852adce.chunk.js"
+  },
+  {
+    "revision": "5f9d3225fc9a71e1fc595feea461c70e",
+    "url": "/greek/static/media/speaker.5f9d3225.png"
+  },
+  {
+    "revision": "e45ef0d1e01a6968748796e30fb2f37d",
+    "url": "/greek/static/media/alpha.e45ef0d1.mp3"
+  },
+  {
+    "revision": "a42e1ad4f0a9b385755b3f517dda61db",
+    "url": "/greek/static/media/beeta.a42e1ad4.mp3"
+  },
+  {
+    "revision": "08c069fa9923e1e3b76e11b5d8619370",
+    "url": "/greek/static/media/gamma.08c069fa.mp3"
+  },
+  {
+    "revision": "8b65ee9305d5c1c86aa1e501378f1976",
+    "url": "/greek/static/media/delta.8b65ee93.mp3"
+  },
+  {
+    "revision": "3598ded66b941ae5f39a8d3a435f4ca1",
+    "url": "/greek/static/media/epsilon.3598ded6.mp3"
+  },
+  {
+    "revision": "e1ffdd9d593df5bd1f39dffa7e8b677c",
+    "url": "/greek/static/media/zeeta.e1ffdd9d.mp3"
+  },
+  {
+    "revision": "72c489c0960918103018745ef7ec5c30",
+    "url": "/greek/static/media/eeta.72c489c0.mp3"
+  },
+  {
+    "revision": "ea93a6730ab2e9ffba58632325f8126b",
+    "url": "/greek/static/media/theta.ea93a673.mp3"
+  },
+  {
+    "revision": "f412a48d39106032b457b35855bbe3e3",
+    "url": "/greek/static/media/iota.f412a48d.mp3"
+  },
+  {
+    "revision": "e39b1da5e33c792686d56d235f5a8b68",
+    "url": "/greek/static/media/kappa.e39b1da5.mp3"
+  },
+  {
+    "revision": "2ea0f9f77a2181008a56ccbdbbdba422",
+    "url": "/greek/static/media/lambda.2ea0f9f7.mp3"
+  },
+  {
+    "revision": "6704a89e6f0384d4209459ea4bf21bfc",
+    "url": "/greek/static/media/mee.6704a89e.mp3"
+  },
+  {
+    "revision": "0697ae3123a1aaee532c2ac88fb5e655",
+    "url": "/greek/static/media/nee.0697ae31.mp3"
+  },
+  {
+    "revision": "9ada40fb76053c6ecdee99875ecb6030",
+    "url": "/greek/static/media/ksee.9ada40fb.mp3"
+  },
+  {
+    "revision": "c3b6e44e8c3727ee6b40549e4be5fe8e",
+    "url": "/greek/static/media/omikron.c3b6e44e.mp3"
+  },
+  {
+    "revision": "f28b95206c72125fc6c957ca1f15c5e0",
+    "url": "/greek/static/media/pee.f28b9520.mp3"
+  },
+  {
+    "revision": "c50eecf725114a9273d4193585e71b6a",
+    "url": "/greek/static/media/row.c50eecf7.mp3"
+  },
+  {
+    "revision": "35cb9126ddfa94c0d2d2346d29c24f3e",
+    "url": "/greek/static/media/sigma.35cb9126.mp3"
+  },
+  {
+    "revision": "71d2c3242bea7f2f233db4499e472c0d",
+    "url": "/greek/static/media/taf.71d2c324.mp3"
+  },
+  {
+    "revision": "4a9cbd11dd70010c6d8acfa4bf5cff4c",
+    "url": "/greek/static/media/ypsilon.4a9cbd11.mp3"
+  },
+  {
+    "revision": "3b5c4ca8e6db3ae8f34ad85ef0f6eb22",
+    "url": "/greek/static/media/fee.3b5c4ca8.mp3"
+  },
+  {
+    "revision": "c6c4a0b48bb64c474cce5ebb3c2c3443",
+    "url": "/greek/static/media/chee.c6c4a0b4.mp3"
+  },
+  {
+    "revision": "3fae403b02c0961d8b3472b48f358ae5",
+    "url": "/greek/static/media/psee.3fae403b.mp3"
+  },
+  {
+    "revision": "5ca27771594067c6e6b74713815e9f9e",
+    "url": "/greek/static/media/omega.5ca27771.mp3"
+  },
+  {
+    "revision": "83303b4845656a2edbdff09154bd1ebe",
+    "url": "/greek/static/media/Anim01.83303b48.gif"
+  },
+  {
+    "revision": "62f828a868f87afe2b47721ed0ddc802",
+    "url": "/greek/static/media/Anim02.62f828a8.gif"
+  },
+  {
+    "revision": "d52da0ee57165b3ab2ded84304287925",
+    "url": "/greek/static/media/Anim03.d52da0ee.gif"
+  },
+  {
+    "revision": "c8ecb9b5300396486f1fab2f693eba13",
+    "url": "/greek/static/media/Anim04.c8ecb9b5.gif"
+  },
+  {
+    "revision": "afc10846d7b1fb9bf2d703f00fd26eab",
+    "url": "/greek/static/media/Anim05.afc10846.gif"
+  },
+  {
+    "revision": "b48c75e2cbc187c25e89f47546f00d21",
+    "url": "/greek/static/media/Anim06.b48c75e2.gif"
+  },
+  {
+    "revision": "0529c40c6da8fe10e1371e9f58bf8582",
+    "url": "/greek/static/media/Anim07.0529c40c.gif"
+  },
+  {
+    "revision": "c760e7cfa8c67d837bcbfa0a61098dca",
+    "url": "/greek/static/media/Anim08.c760e7cf.gif"
+  },
+  {
+    "revision": "aaf81d6914b328debd71c7011ef65016",
+    "url": "/greek/static/media/Anim09.aaf81d69.gif"
+  },
+  {
+    "revision": "afed06c44533589291fdc48854c28d3b",
+    "url": "/greek/static/media/Anim10.afed06c4.gif"
+  },
+  {
+    "revision": "4fdd942419c1e27ef3ef",
+    "url": "/greek/static/js/main.509325d6.chunk.js"
+  },
+  {
+    "revision": "6bc8aa723fcec151f76683c25dc14e03",
+    "url": "/greek/static/media/Anim12.6bc8aa72.gif"
+  },
+  {
+    "revision": "70beead606e70808935af6b5754d7618",
+    "url": "/greek/static/media/Anim13.70beead6.gif"
+  },
+  {
+    "revision": "15d097d53fae62257f859ae3cbc78637",
+    "url": "/greek/static/media/Anim14.15d097d5.gif"
+  },
+  {
+    "revision": "8f4d33035fee07af11a90726625c45ff",
+    "url": "/greek/static/media/Anim15.8f4d3303.gif"
+  },
+  {
+    "revision": "506e181e89b65d5be8e90d5b8cb55cda",
+    "url": "/greek/static/media/Anim16.506e181e.gif"
+  },
+  {
+    "revision": "762ebc788e6f982284d76e7a16fb37de",
+    "url": "/greek/static/media/Anim17.762ebc78.gif"
+  },
+  {
+    "revision": "740c8691592f557ed96bcdaa799ddea8",
+    "url": "/greek/static/media/Anim18.740c8691.gif"
+  },
+  {
+    "revision": "723ba01123d7528e9f91c7f1651e99b7",
+    "url": "/greek/static/media/Anim19.723ba011.gif"
+  },
+  {
+    "revision": "14795bd4e6f78cfc814b8c0db9b5313b",
+    "url": "/greek/static/media/Anim20.14795bd4.gif"
+  },
+  {
+    "revision": "fb1cf63595299075a40241634fe5141f",
+    "url": "/greek/static/media/Anim21.fb1cf635.gif"
+  },
+  {
+    "revision": "1e4a821e53a531a24ae61d4ff4464f2c",
+    "url": "/greek/static/media/Anim22.1e4a821e.gif"
+  },
+  {
+    "revision": "9817a559fe1a24bc9c8ba9502af80a3d",
+    "url": "/greek/static/media/Anim23.9817a559.gif"
+  },
+  {
+    "revision": "f085003c2f2c0110d9b5ce85d8b00237",
+    "url": "/greek/static/media/Anim24.f085003c.gif"
+  },
+  {
+    "revision": "b0e00d33746564efd615ae6c623cbce7",
+    "url": "/greek/static/media/alpha.b0e00d33.mp3"
+  },
+  {
+    "revision": "49c3e5ce179bc8878b6e05a93e4a73f1",
+    "url": "/greek/static/media/beeta.49c3e5ce.mp3"
+  },
+  {
+    "revision": "4bb96ced966148ee5d1e8f6b8a3bce69",
+    "url": "/greek/static/media/gamma.4bb96ced.mp3"
+  },
+  {
+    "revision": "adb4c70ead3a2b90d17ee76ead4c495b",
+    "url": "/greek/static/media/delta.adb4c70e.mp3"
+  },
+  {
+    "revision": "bac2b832f41d4eb1e3b1f04220ea5ad6",
+    "url": "/greek/static/media/epsilon.bac2b832.mp3"
+  },
+  {
+    "revision": "534765c93ea0d620e9c900fd2b55e541",
+    "url": "/greek/static/media/zeeta.534765c9.mp3"
+  },
+  {
+    "revision": "3c1acc55c33038df128a52614bf05803",
+    "url": "/greek/static/media/eeta.3c1acc55.mp3"
+  },
+  {
+    "revision": "abc558d7645df2d71142546e60d6dc3d",
+    "url": "/greek/static/media/theta.abc558d7.mp3"
+  },
+  {
+    "revision": "31228eba911af8e5efcdfc7e6ee235ea",
+    "url": "/greek/static/media/iota.31228eba.mp3"
+  },
+  {
+    "revision": "aab4d89cea34e6b07385c505f62ac60a",
+    "url": "/greek/static/media/kappa.aab4d89c.mp3"
+  },
+  {
+    "revision": "f1ce31aee87b147c4ec1b4a37ff09f50",
+    "url": "/greek/static/media/lambda.f1ce31ae.mp3"
+  },
+  {
+    "revision": "8bca963c01baecc6848f04a50cbe9e7a",
+    "url": "/greek/static/media/mee.8bca963c.mp3"
+  },
+  {
+    "revision": "14e16c3bcec7c84ccb083770d095a255",
+    "url": "/greek/static/media/nee.14e16c3b.mp3"
+  },
+  {
+    "revision": "d915eeabf361b487ad14e0795275a6b3",
+    "url": "/greek/static/media/ksee.d915eeab.mp3"
+  },
+  {
+    "revision": "1ab40c8cd8d4887267d89e6d5478f154",
+    "url": "/greek/static/media/omikron.1ab40c8c.mp3"
+  },
+  {
+    "revision": "f25a2afecbc0a31ece1ec184174c7fcb",
+    "url": "/greek/static/media/pee.f25a2afe.mp3"
+  },
+  {
+    "revision": "d0d2cdfe922b9fe451e9a062b64630e4",
+    "url": "/greek/static/media/row.d0d2cdfe.mp3"
+  },
+  {
+    "revision": "78191dc579a46c79f43726947b878075",
+    "url": "/greek/static/media/sigma.78191dc5.mp3"
+  },
+  {
+    "revision": "3872c0d13a42e14409ccf5142b7f753a",
+    "url": "/greek/static/media/taf.3872c0d1.mp3"
+  },
+  {
+    "revision": "6e9259629d2b47f6c0348a5f38faf4b5",
+    "url": "/greek/static/media/ypsilon.6e925962.mp3"
+  },
+  {
+    "revision": "5482cfcc93acee0e9349c24cc273625d",
+    "url": "/greek/static/media/fee.5482cfcc.mp3"
+  },
+  {
+    "revision": "6fc0402184a75a1f45aceb01a833c69a",
+    "url": "/greek/static/media/chee.6fc04021.mp3"
+  },
+  {
+    "revision": "b3d53724aaa2f4faa96863b7476516ca",
+    "url": "/greek/static/media/psee.b3d53724.mp3"
+  },
+  {
+    "revision": "16d579cc189c34ca0ccf53d1fa1fa937",
+    "url": "/greek/static/media/omega.16d579cc.mp3"
+  },
+  {
+    "revision": "dcdb5a81bd22df2607251f288f6a3c54",
+    "url": "/greek/index.html"
+  }
+];
